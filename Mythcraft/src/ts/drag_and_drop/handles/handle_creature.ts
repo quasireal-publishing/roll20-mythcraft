2eef256dc28ebc33d5d7e568f32145f1acd60458
Sean <sean.odell813@gmail.com>
2025-10-07 18:40:10 -0700

    updated attrs and logic for `handle_creatures.ts`

--- Mythcraft/src/ts/drag_and_drop/handles/handle_creature.ts
+++ Mythcraft/src/ts/drag_and_drop/handles/handle_creature.ts
@@ -1,12 +1,48 @@
 const handle_creature = (page: CompendiumAttributes) => {
   //This will only handle top level attributes, not repeating sections (arrays with objects)
   //Add more attributes as needed.
-  const attrs = ["awareness", "charisma"];
+  const attrs = [
+    "name",
+    "description",
+    "level",
+    "size",
+    "strength",
+    "dexterity",
+    "endurance",
+    "awareness",
+    "intellect",
+    "charisma",
+    "reflexes",
+    "fortitude",
+    "anticipation",
+    "logic",
+    "willpower",
+    "hit_points",
+    "armor_rating",
+    "speed",
+    "dr",
+    "senses",
+    "action_description",
+    //Array attrs
+    "resist",
+    "immune",
+    "vulnerable",
+    "traits"
+  ];
   const update = getUpdate(attrs, page);
 
   update["character_name"] = page.name;
 
-  //Write a repeating sections (arrays with objects) similar to this.
+  if (page.data.skills && Array.isArray(page.data.skills)) {
+    page.data.skills.forEach(
+      (skill: { name: string; attribute: string; bonus: string }) => {
+        const skillRow = getRow("skills");
+        update[`${skillRow}_name`] = skill.name;
+        update[`${skillRow}_attribue`] = skill.attribute;
+        update[`${skillRow}_bonus`] = skill.bonus;
+      }
+    )
+  }
   if (page.data.features && Array.isArray(page.data.features)) {
     page.data.features.forEach(
       (feature: { name: string; description: string }) => {
@@ -15,6 +51,19 @@ const handle_creature = (page: CompendiumAttributes) => {
         update[`${featureRow}_description`] = feature.description;
       }
     );
+  }
+  if (page.data.actions && Array.isArray(page.data.actions)) {
+    page.data.actions.forEach(
+      (action: { name: string; range: string; type: string; bonus: string; defense: string; description: string }) => {
+        const actionRow = getRow("actions");
+        update[`${actionRow}_name`] = action.name;
+        update[`${actionRow}_range`] = action.range;
+        update[`${actionRow}_type`] = action.type;
+        update[`${actionRow}_bonus`] = action.bonus;
+        update[`${actionRow}_defense`] = action.defense;
+        update[`${actionRow}_description`] = action.description;
+      }
+    )
   }
 
   try {
